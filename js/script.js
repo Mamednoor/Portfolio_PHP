c29078e0e80cc0eb711d604d26589cbf5d62c59f
Mamednoor <mamed.gohabur@gmail.com>
2021-05-31 23:22:21 +0200

    SP, update project

--- js/script.js
+++ js/script.js
@@ -36,3 +36,36 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+document.getElementById("image").addEventListener("change", function(evt){
+  var p=document.getElementById("preview"); /* Bloc d'affichage de la liste des fichiers */
+  p.innerHTML=""; /* Effacer le contenu initial de #preview */
+  for (var i=0; i<this.files.length; i++) {
+    var f=this.files[i];
+    var div=document.createElement("div");
+    div.className="image";
+    var span=document.createElement("span");
+    span.innerHTML=f.name+" ("+getHumanSize(f.size)+")";
+    var vignette=document.createElement("img");
+    vignette.src = window.URL.createObjectURL(f); 
+    /* Attacher les élements HTML au DOM */
+    div.appendChild(vignette);
+    div.appendChild(span);
+    p.appendChild(div);
+  }
+  p.style.display="flex";  
+  img.style="flex"
+});
+/* Retourne une taille de fichier en mode lisible par un humain */
+function getHumanSize(s) {
+  s=parseInt(s); /* Pour s'assurer que le paramètre d'entrée est entier */
+  if (s<1024) {
+    return s+" o";  
+  } else if (s<1024*1024) {
+    return (s/1024).toFixed(1)+" ko";  
+  } else if (s<1024*1024*1024) {
+    return (s/1024/1024).toFixed(1)+" Mo";  
+  } else {
+    return (s/1024/1024/1024).toFixed(1)+" Go";  
+  }
+}
