ddf47bac991b66649fca1c35f2ace5b22b96a4ac
Mamednoor <mamed.gohabur@gmail.com>
2021-06-09 23:06:46 +0200

    SP

--- frontend/experience.php
+++ frontend/experience.php
@@ -1,8 +1,13 @@
 <section class="experience" id="experiences">
   <div class="row">
     <h2>Expériences</h2>
-    <!-- Add logos of the experiences or companies you'have worked with. -->
 
+      <fieldset style="border: 2px solid #ff3258">
+        <legend style="font-size: x-large;" align="center">Work in Progress</legend>
+        <div style="padding-top: 100px; padding-bottom: 100px;">
+          <blockquote style="text-align: center;">Cette partie est en cours de construction</blockquote>
+        </div>
+      </fieldset>
 
   </div>
   </div>
