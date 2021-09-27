da08ba25fcae0a034e0579f1f39ddeb2d0e01865
Mamednoor <mamed.gohabur@gmail.com>
2021-09-27 16:27:48 +0200

    correction link contact linkedin

--- frontend/footer.php
+++ frontend/footer.php
@@ -3,39 +3,18 @@
     <ul class="footer_social-links">
 
       <li class="footer_social-link-item">
-        <a
-          href="https://github.com/Mamednoor"
-          title="Link to Github Profile"
-          target="_blank"
-        >
-          <img
-            src="asset/images/github.svg"
-            class="footer_social-image"
-            alt="Github"
-          />
+        <a href="https://github.com/Mamednoor" title="Link to Github Profile" target="_blank">
+          <img src="asset/images/github.svg" class="footer_social-image" alt="Github" />
         </a>
       </li>
       <li class="footer_social-link-item">
-        <a
-          href="https://codepen.io/mamednoor"
-          title="Link to Codepen Profile"
-          target="_blank"
-        >
-          <img
-            src="asset/images/codepen.svg"
-            class="footer_social-image"
-            alt="Codepen"
-          />
+        <a href="https://codepen.io/mamednoor" title="Link to Codepen Profile" target="_blank">
+          <img src="asset/images/codepen.svg" class="footer_social-image" alt="Codepen" />
         </a>
       </li>
       <li class="footer_social-link-item">
-        <a href="https://www.linkedin.com/in/mamednoor/" target="_blank">
-          <img
-            src="asset/images/linkedin.svg"
-            title="Link to Linkedin Profile"
-            class="footer_social-image"
-            alt="Linkedin"
-          />
+        <a href="https://fr.linkedin.com/in/mamednoorgohabur" target="_blank">
+          <img src="asset/images/linkedin.svg" title="Link to Linkedin Profile" class="footer_social-image" alt="Linkedin" />
         </a>
       </li>
     </ul>
